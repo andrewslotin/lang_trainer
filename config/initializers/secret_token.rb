78f10682c0a0e42806723753a473c7d921c27bdc
Andrew Slotin <andrew.slotin@gmail.com>
2012-11-12 17:22:37 +0400

    Initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LangTrainer::Application.config.secret_token = '0c70a4c08bfdea3e1bca8dde646ec8cb1ab959af41c4c9634c080fb5372cc5020e39b744fa0d391bf769a8f6fa21b5eba3df809de4eb671a4e0a87fdf23b5e13'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+LangTrainer::Application.config.session_store :cookie_store, key: '_lang_trainer_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# LangTrainer::Application.config.session_store :active_record_store
