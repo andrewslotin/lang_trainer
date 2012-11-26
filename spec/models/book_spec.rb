aa5d4326924e1a2ec4bdad625f8e984d0f398ff5
Andrew Slotin <andrew.slotin@gmail.com>
2012-11-26 16:17:50 +0400

    Book spec

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -2,9 +2,27 @@
 require "spec_helper"
 
 describe Book do
-  describe "ignore" do
+  subject { FactoryGirl.create :book }
+  let(:word) { "the" }
+
+  describe "ignore_word" do
     context "for each chapter in book" do
-      it "calls Chapter#ignore with given word"
+      let(:chapters) do
+        (0...3).map { |i| FactoryGirl.build :chapter }
+      end
+
+      before do
+        subject.chapters = chapters
+        subject.save!
+      end
+
+      it "calls Chapter#ignore_word with given word" do
+        chapters.each do |chapter|
+          chapter.should_receive :ignore_word, with: word
+        end
+
+        subject.ignore_word word
+      end
     end
   end
 end
