9852e87e5015c8679f0632527b1ca8a28a049682
Andrew Slotin <andrew.slotin@gmail.com>
2012-11-30 17:39:15 +0400

    Reworked book view

--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,7 @@ LangTrainer::Application.routes.draw do
   end
 
   resources :books do
-    resources :chapters do
+    resources :chapters, except: [:index] do
       resources :entries do
         put :ignore, on: :member
         put :complete, on: :member
