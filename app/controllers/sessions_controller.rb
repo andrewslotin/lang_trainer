41ce4ec27ac40976e37dbf6f539e365ffeee52bd
Andrew Slotin <andrew.slotin@gmail.com>
2012-11-30 17:55:20 +0400

    Debug for heroku

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
 
   def create
     auth = request.env["omniauth.auth"]
-    render json: request.env
+    render json: request
     return
     user = User.where("identities.provider" => auth['provider'], "identities.uid" => auth["uid"].to_s).first || User.create_with_omniauth(auth)
 
